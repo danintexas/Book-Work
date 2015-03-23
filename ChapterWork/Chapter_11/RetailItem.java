b7488f1228cd7a344138814ccb7d1316747cc56c
Daniel Gail <daniel.gail@gmail.com>
2015-03-23 07:08:39 -0500

    Chapter Work
    Completed Chapter 11

---
+++ ChapterWork/Chapter_11/RetailItem.java
@@ -0,0 +1,16 @@
+/*
+Daniel Gail
+March 23, 2015
+
+Chapter 11
+Code Listing 11-32
+*/
+
+/**
+ * RetailItem interface
+ */
+
+public interface RetailItem 
+{
+    public double getRetailPrice();
+}
