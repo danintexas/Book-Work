21f06315d61d55058e06117786609132e717edd4
Daniel Gail <daniel.gail@gmail.com>
2015-05-24 21:35:04 -0500

    Chapter Work

---
+++ Programming Challenges/Chapter_02/Challenge_2_04.java
@@ -0,0 +1,20 @@
+/*
+Daniel Gail
+Sept 22, 2014
+Chapter 2 Challenge 4
+Using the * character print out a diamond pattern
+ */
+
+public class Challenge_2_04 
+{
+    public static void main (String [] args)
+    {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+        System.out.println(" *****");
+        System.out.println("  ***");
+        System.out.println("   *");
+    }
+}
---
+++ Programming Challenges/Chapter_02/Challenge_2_07.java
@@ -0,0 +1,20 @@
+// Daniel Gail
+// Sept 23, 2014
+// Chapter 2 Challenge 07
+
+// Calculate the number of acres in a tract of land
+// 1 acre = 43,560 square feet
+
+public class Challenge_2_07
+{
+   public static void main (String [] args)
+   {
+      float acre = 43560, landSize = 389767, value;
+      
+      value = landSize / acre;
+      
+      System.out.print("The size of the property is: " + (int) landSize + "\nThe number of " + 
+         "acres in this property is: " + value);   
+      
+   }
+}      
